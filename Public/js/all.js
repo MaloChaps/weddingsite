63dbb90177f33710f2d8e5fef1a42f5fa9c077be
malo chaperon <chaperon.malo@gmail.com>
2021-02-20 22:54:27 +0100

    all

---
+++ Public/js/all.js
@@ -0,0 +1,210 @@
+// scroll reveal anime
+const sr = ScrollReveal();
+
+// dist title
+
+const delayTitle = 500;
+const durationTitle = 1500;
+
+sr.reveal('.cta-dark', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.cta-light', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+sr.reveal('.h1-dark', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h1-light', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h2-dark', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h2-ligth', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h3-dark', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+sr.reveal('.h3-light', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h4-dark', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.h4-light', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.body-dark', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.titleProject-dark', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.titleProject-light', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+sr.reveal('.midTitleProject-dark', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+sr.reveal('.midTitleProject-light', {
+	 delay: delayTitle,
+    duration: durationTitle,
+    distance: "100px",
+    origin: "bottom"
+});
+
+
+//  COMPOSANTS SCROLL REVEAL 
+const delayComposants = 200;
+const durationComposants = 1300;
+const distanceComposants = 70;
+
+sr.reveal('.home-section', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+sr.reveal('.textAbout-section', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+sr.reveal('.favoritProject-section', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+
+sr.reveal('.photo-content-section', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+
+sr.reveal('.middle', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+
+sr.reveal('.full', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+sr.reveal('.min', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+
+sr.reveal('.allProjects-section', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+sr.reveal('.contentProject-section', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+sr.reveal('.contactNow-section', {
+	 delay: delayComposants,
+    duration: durationComposants,
+    distance: distanceComposants + 'px',
+    origin: "bottom"
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
